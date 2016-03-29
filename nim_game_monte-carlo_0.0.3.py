46f31cc19cfa189514ad367b9d42cea3bfc7efe0
hinamiqi <hinamiqi@gmail.com>
2016-03-29 21:30:50 +0400

    +

---
+++ nim_game_monte-carlo_0.0.3.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 18:21:26 2016
+
+@author: Modza
+
+v. 0.0.3
+"""
+
+import random
+import argparse
+
+def turn_state(state):
+	if state == comp_turn:
+		return "It's computer turn."
+	else:
+		return "It's your turn."
+
+def draw(state):
+	print("Number of stones: ", state['stones'])
+	print(turn_state(state['turn']))
+
+def player_turn(state):
+	print("How much to take (1,2,3) ?")
+	return [int(input()), comp_turn]
+
+def comp_turn(state):
+	moves_variants = list(map(lambda x,y: x-y, [state for i in range(3)], [1,2,3]))
+	print('variants: ', moves_variants)
+	res_list = (list(map(monte_carlo, moves_variants)))
+	print(res_list)
+	#res_list.reverse()
+	print(min(res_list))
+	print(res_list.index(min(res_list)))
+	res = res_list.index(min(res_list))+1
+	print("Computer takes ", res, " stones.")
+	return [res, player_turn]
+
+def how_much_left(x,y):
+	return x-y
+
+def check(state):
+	if state['stones'] == 1:
+		if state['turn'] == comp_turn:
+			return 'Computer lose!'
+		elif state['turn'] == player_turn:
+			return 'You lose!'
+		else:
+			return state['turn']
+	elif state['stones'] == 0:
+		return 'You lose!'
+
+def monte_carlo(state, K=1000):
+	games = [state for game in range(K)]
+	return list(map(random_moves, games)).count(True)
+
+def random_moves(number, turn=False):
+	if number <= 1:
+		return turn
+	else:
+		
+		return random_moves((number - random.choice([1,2,3])), (not turn))
+		
+
+def game(state):
+	draw(state)
+	if check(state):
+		print(check(state))
+	else:
+		res = state['turn'](state['stones'])
+		game({'stones': (state['stones'] - res[0]), 'turn': res[1]})
+
+def arg_parse_func():
+    parser = argparse.ArgumentParser(description='Nim game')
+    parser.add_argument('-s', '--stones', type=int, help='the number of stones')
+    parser.add_argument('-p', '--player', action='store_true', help='player goes first')
+    args = parser.parse_args()
+    return {'stones': (args.stones or 10), 'turn': ((args.player and player_turn) or (comp_turn))}
+
+
+#init
+game(arg_parse_func())
--- platformer/main.py
+++ platformer/main.py
@@ -2,203 +2,112 @@
 # -*- coding: UTF-8 -*-
 
 import pyglet
-import sys
-from pyglet.gl import *
 from pyglet.window import key
-
-backgroundColor = [176, 224, 230, 255]
-playerColor = [107, 142, 35]
-blockColor = [184, 134, 11]
-
-CollideObjects = []
+from pyglet import clock
+from pyglet.gl import *
 
 class Actor(object):
-	def __init__(self, color, start_x, start_y, width, height):
-		self.color = color
-		self.x = start_x
-		self.y = start_y
-		self.width = width
-		self.height = height
-		self.speed = 50
-		self.vel = 0
-		self.msg = ""
-	
-	def vert(self):
-		verts = [self.x, self.y, \
-						self.x + self.width, self.y, \
-						self.x + self.width, self.y + self.height, \
-						self.x, self.y + self.height]
-		
-		return verts
-	
-	def draw(self):
-		r, g, b = rgb_to_pyglet(self.color)
-		glColor3f(r, g, b)
-		pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, \
-						('v2i', self.vert()))
-	
-	# def move_up(self):
-		# if self.msg != 'Top!':
-			# self.y += self.speed
-	
-	# def move_down(self):
-		# if self.msg != 'Bot!':
-			# self.y -= self.speed
-		
-	# def move_left(self):
-		# if self.msg != 'Left!':
-			# self.x -= self.speed
-		
-	# def move_right(self):
-		# if self.msg != 'Right!':
-			# self.x += self.speed
-			
-	def move_up(self):
-		if self.msg != 'Top!':
-			self.y += self.vel
-	
-	def move_down(self):
-		if self.msg != 'Bot!':
-			self.y -= self.vel
-		
-	def move_left(self):
-		if self.msg != 'Left!':
-			self.x -= self.vel
-		
-	def move_right(self):
-		if self.msg != 'Right!':
-			self.x += self.vel
-			
-	
-	def jump(self):
-		if self.msg != 'Top!':
-			self.y += self.vel
-		
-		
-	def CollisionCheck(self, block):
-		if self.x + self.width == block.x and \
-			self.y + self.height > block.y and \
-			self.y < block.y + block.height:
-			self.msg = "Right!"
-			return True
-		
-		elif self.x == block.x + block.width and \
-			self.y + self.height > block.y and \
-			self.y < block.y + block.height:
-			self.msg = "Left!"
-			return True
-			
-		elif self.y + self.height == block.y and \
-			self.x + self.width > block.x and \
-			self.x < block.x + block.width:
-			self.msg = "Top!"
-			return True
-			
-		elif self.y == block.y + block.height and \
-			self.x + self.width > block.x and \
-			self.x < block.x + block.width:
-			self.msg = "Bot!"
-			return True
-		else:
-			self.msg = ""
-	
-	
-	def update(self, dt):
+    def __init__(self, color, start_x, start_y, width, height):
+        self.color = color
+        self.x = start_x
+        self.y = start_y
+        self.width = width
+        self.height = height
+        self.speed = 50
+        self.vel = 0
+        self.msg = ""
 
-		if self.vel > 0:
-			print(self.vel)
-			self.vel -= 1
-		elif self.vel < 0:
-			self.vel = 0
-		
-		for key in keys_held:
-			if key == pyglet.window.key.UP:
-				self.vel = self.speed
-				self.move_up()
-			elif key == pyglet.window.key.DOWN:
-				self.vel = self.speed
-				self.move_down()
-			elif key == pyglet.window.key.RIGHT:
-				self.vel = self.speed
-				self.move_right()
-			elif key == pyglet.window.key.LEFT:
-				self.vel = self.speed
-				self.move_left()
-			elif key == pyglet.window.key.SPACE:
-				self.vel = self.speed * 2
-				self.jump()
-	
+    def vert(self):
+        verts = [self.x, self.y,
+                 self.x + self.width, self.y,
+                 self.x + self.width, self.y + self.height,
+                 self.x, self.y + self.height]
+        return verts
+
+    def draw(self):
+#        r, g, b = self.rgb_to_pyglet(self.color)
+        glColor3f(*self.color)
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, \
+                        ('v2i', self.vert()))
+
+    def CollisionCheck(self, block):
+        if self.x + self.width == block.x and \
+            self.y + self.height > block.y and \
+            self.y < block.y + block.height:
+            self.msg = "Right!"
+            return True
+
+        elif self.x == block.x + block.width and \
+            self.y + self.height > block.y and \
+            self.y < block.y + block.height:
+            self.msg = "Left!"
+            return True
+
+        elif self.y + self.height == block.y and \
+            self.x + self.width > block.x and \
+            self.x < block.x + block.width:
+            self.msg = "Top!"
+            return True
+
+        elif self.y == block.y + block.height and \
+            self.x + self.width > block.x and \
+            self.x < block.x + block.width:
+            self.msg = "Bot!"
+            return True
+        else:
+            self.msg = ""
+
+
+    def update(self):
+        pass
+
 
 def rgb_to_pyglet(list):
-	result = []
-	for i in list:
-		result.append(i/255)
-	return result[0], result[1], result[2]
-	
-def clr_format(value):
-	return value/255
+    result = []
+    for i in list:
+        result.append(i/255)
+    return result[0], result[1], result[2]
 
-window = pyglet.window.Window(512, 480)
-r, g, b = rgb_to_pyglet(backgroundColor)
-pyglet.gl.glClearColor(r, g, b, 1)
-player = Actor(playerColor, 50, 50, 10, 30)
-block = Actor(blockColor, 200, 200, 100, 100)
-CollideObjects.append(block)
-keys_held = []
-label = pyglet.text.Label("",
-                          font_name='Arial',
-                          font_size=20, x=10, y=440,
-                          anchor_x='left', anchor_y='bottom')
-						  
+class App(pyglet.window.Window):
+
+    def __init__(self, color):
+        pyglet.window.Window.__init__(self, 512, 480)
+        self.color = color
+        
+        clock.set_fps_limit(60)
+        
+    def on_draw(self):
+        glClearColor(*self.color, 1)
+        
+        self.clear()
+        
+    def mainLoop(self):
+        pyglet.app.run()
+            
+            #self.draw()
 
 
-@window.event
-def on_draw():
-	window.clear()
-	# r, g, b = rgb_to_pyglet(playerColor)
-	# glColor3f(r, g, b)
-	# #verts = [10, 10, 10, 100, 100, 100, 100, 10]
-	# verts = player.update()
-	# pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, \
-						# ('v2i', verts))
-	player.draw()
-	block.draw()
-	if player.CollisionCheck(block):
-		label.text = player.msg
-		label.draw()
-	
-	
 
-@window.event
-def on_key_press(key, modifiers):
-	keys_held.append(key)
 
-	# if key == pyglet.window.key.UP:
-		# player.move_up()
-	# elif key == pyglet.window.key.DOWN:
-		# player.move_down()
-	# elif key == pyglet.window.key.RIGHT:
-		# player.move_right()
-	# elif key == pyglet.window.key.LEFT:
-		# player.move_left()
 
-@window.event
-def on_key_release(key, modifiers):
-	keys_held.pop(keys_held.index(key))
-	
 
-	# for key in keys_held:
-		# if key == pyglet.window.key.UP:
-			# player.move_up()
-		# elif key == pyglet.window.key.DOWN:
-			# player.move_down()
-		# elif key == pyglet.window.key.RIGHT:
-			# player.move_right()
-		# elif key == pyglet.window.key.LEFT:
-			# player.move_left()
-		# elif key == pyglet.window.key.SPACE:
-			# vel = 10
-	
 
-pyglet.clock.schedule_interval(player.update, 1/60)
-pyglet.app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
