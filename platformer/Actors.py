677ad159e8cd4181ec0f3be9eda18f8506924bd1
hinamiqi <hinamiqi@gmail.com>
2016-03-30 20:52:35 +0400

    Smooth movements. Collisions.
    Smooth movements = OK
    Collisions: does not work properly

--- platformer/Actors.py
+++ platformer/Actors.py
@@ -5,71 +5,88 @@ import pyglet
 
 class Actor(object):
 
-	def __init__(self, color, start_x, start_y, width, height):
-		self.color = color
-		self.x = start_x
-		self.y = start_y
-		self.width = width
-		self.height = height
-		self.speed = 50
-		self.vel = 0
-		self.collide = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
+    def __init__(self, color, start_x, start_y, width, height):
+        self.color = color
+        self.x = start_x
+        self.y = start_y
+        self.width = width
+        self.height = height
 
-	def vert(self):
-		verts = [self.x, self.y,
-				 self.x + self.width, self.y,
-				 self.x + self.width, self.y + self.height,
-				 self.x, self.y + self.height]
-		return verts
+    def vert(self):
+        verts = [self.x, self.y,
+                 self.x + self.width, self.y,
+                 self.x + self.width, self.y + self.height,
+                 self.x, self.y + self.height]
+        return verts
 
-	def draw(self):
-#		 r, g, b = self.rgb_to_pyglet(self.color)
-		pyglet.gl.glColor3f(*self.color)
-		pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, \
-						('v2i', self.vert()))
+    def draw(self):
+#         r, g, b = self.rgb_to_pyglet(self.color)
+        pyglet.gl.glColor3f(*self.color)
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, \
+                        ('v2i', self.vert()))
 
 class Player(Actor):
-	def __init__(self, color, start_x, start_y, width, height):
-		super().__init__(color, start_x, start_y, width, height)
-		self.delta_x = 0
-		self.delta_y = 0
+    def __init__(self, color, start_x, start_y, width, height):
+        super().__init__(color, start_x, start_y, width, height)
+        self.delta_x = 0
+        self.delta_y = 0
+        self.max_speed = 10
+        self.up_vel = 0
+        self.down_vel = 0
+        self.left_vel = 0
+        self.right_vel = 0
+        self.collide = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
 
-	def CollisionCheck(self, block):
-		if self.x + self.width == block.x and \
-			self.y + self.height > block.y and \
-			self.y < block.y + block.height:
-			self.collide['Up'] = True
-
-
-		elif self.x == block.x + block.width and \
-			self.y + self.height > block.y and \
-			self.y < block.y + block.height:
-			self.collide['Left'] = True
-
-
-		elif self.y + self.height == block.y and \
-			self.x + self.width > block.x and \
-			self.x < block.x + block.width:
-			self.collide['Top!'] = True
+    def CollisionCheck(self, block):
+        if self.x + self.width >= block.x and \
+            self.y + self.height >= block.y and \
+            self.y <= block.y + block.height:
+            self.collide['Right'] = True
 
 
-		elif self.y == block.y + block.height and \
-			self.x + self.width > block.x and \
-			self.x < block.x + block.width:
-			self.collide['Bot'] = True
-
-		else:
-			self.collide = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
-
-	def Moving(self):
-		
-		if self.delta_x != 0:
-			self.x = self.x + self.delta_x
-		if self.delta_y != 0:
-			self.y = self.y + self.delta_y
+        elif self.x <= block.x + block.width and \
+            self.y + self.height >= block.y and \
+            self.y <= block.y + block.height:
+            self.collide['Left'] = True
 
 
+        elif self.y + self.height >= block.y and \
+            self.x + self.width >= block.x and \
+            self.x <= block.x + block.width:
+            self.collide['Up'] = True
 
+
+        elif self.y <= block.y + block.height and \
+            self.x + self.width >= block.x and \
+            self.x <= block.x + block.width:
+            self.collide['Down'] = True
+
+        else:
+            self.collide = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
+
+#    def Moving(self):
+#        
+#        if self.delta_x != 0:
+#            self.x = self.x + self.delta_x
+#        if self.delta_y != 0:
+#            self.y = self.y + self.delta_y
+
+    def move_up(self):
+        if self.collide['Up'] == False:
+            self.y += self.up_vel
+        
+    def move_down(self):
+        if self.collide['Down'] == False:
+            self.y -= self.down_vel
+            
+    def move_right(self):
+        if self.collide['Right'] == False:
+            self.x += self.right_vel
+            
+            
+    def move_left(self):
+        if self.collide['Left'] == False:
+            self.x -= self.left_vel
 
 
 
--- platformer/main.py
+++ platformer/main.py
@@ -4,67 +4,116 @@ from Actors import *
 backgroundColor = [176, 224, 230, 255]
 playerColor = [107, 142, 35]
 blockColor = [184, 134, 11]
- 
+
 class Window(pyglet.window.Window):
- 
-	def __init__(self):
-		super().__init__(512, 480)
-		# pyglet.gl.glColor3f(*rgb_to_pyglet(backgroundColor))
-		pyglet.gl.glClearColor(*rgb_to_pyglet(backgroundColor))
-		pyglet.clock.schedule_interval(self.update, 1/60)
-		self.key_holder = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
-		self.player = Player(rgb_to_pyglet(playerColor), 100, 100, 20, 60)
-		self.block = Actor(rgb_to_pyglet(blockColor), 200, 200, 100, 100)
-		
- 
-	def on_draw(self):
-		self.clear()
-		self.player.draw()
-		self.block.draw()
- 
-	def update(self, dt):
-		self.player.delta_y = 0
-		self.player.delta_x = 0
-		self.player.CollisionCheck(self.block)
-		if self.key_holder['Up']:
-			if self.player.collide['Up'] != True:
-				self.player.delta_y = 10
-		elif self.key_holder['Down']:
-			self.player.delta_y = -10
-		elif self.key_holder['Left']:
-			self.player.delta_x = -10
-		elif self.key_holder['Right']:
-			self.player.delta_x = 10
-		self.player.Moving()
 
-	def on_key_press(self, key, modifiers):
-		if key == pyglet.window.key.UP:
-			self.key_holder['Up'] = True
-		elif key == pyglet.window.key.DOWN:
-			self.key_holder['Down'] = True
-		elif key == pyglet.window.key.RIGHT:
-			self.key_holder['Right'] = True
-		elif key == pyglet.window.key.LEFT:
-			self.key_holder['Left'] = True
+    def __init__(self):
+        super().__init__(512, 480)
+        # pyglet.gl.glColor3f(*rgb_to_pyglet(backgroundColor))
+        pyglet.gl.glClearColor(*rgb_to_pyglet(backgroundColor))
+        pyglet.clock.schedule_interval(self.update, 1/60)
+        self.key_holder = {'Up': False, 'Down': False, 'Left': False, 'Right': False}
+        self.player = Player(rgb_to_pyglet(playerColor), 100, 100, 20, 60)
+        self.block = Actor(rgb_to_pyglet(blockColor), 200, 200, 100, 100)
+        self.label = pyglet.text.Label("", font_name='Arial', font_size=20, 
+                                           x=10, y=440,
+                                           anchor_x='left', anchor_y='bottom')
 
-	def on_key_release(self, key, modifiers):
-		if key == pyglet.window.key.UP:
-			self.key_holder['Up'] = False
-		elif key == pyglet.window.key.DOWN:
-			self.key_holder['Down'] = False
-		elif key == pyglet.window.key.RIGHT:
-			self.key_holder['Right'] = False
-		elif key == pyglet.window.key.LEFT:
-			self.key_holder['Left'] = False
+
+
+    def on_draw(self):
+        self.clear()
+        self.player.draw()
+        self.block.draw()
+        self.label.draw()
+
+    def update(self, dt):
+
+        self.player.CollisionCheck(self.block)
+
+        #move up:
+        if self.key_holder['Up']:
+            if self.player.up_vel < self.player.max_speed:
+                self.player.up_vel += 1
+            else:
+                self.player.up_vel = self.player.max_speed
+
+        else:
+            if self.player.up_vel > 0:
+                self.player.up_vel -= 1
+            else:
+                self.player.up_vel = 0
+        self.player.move_up()
+
+        #move down:
+        if self.key_holder['Down']:
+            if self.player.down_vel < self.player.max_speed:
+                self.player.down_vel += 1
+            else:
+                self.player.down_vel = self.player.max_speed
+        else:
+            if self.player.down_vel > 0:
+                self.player.down_vel -= 1
+            else:
+                self.player.down_vel = 0
+        self.player.move_down()
+
+        #move right:
+        if self.key_holder['Right']:
+            if self.player.right_vel < self.player.max_speed:
+                self.player.right_vel += 1
+            else:
+                self.player.right_vel = self.player.max_speed
+        else:
+            if self.player.right_vel > 0:
+                self.player.right_vel -= 1
+            else:
+                self.player.right_vel = 0
+        self.player.move_right()
+
+        #move left:
+        if self.key_holder['Left']:
+            if self.player.left_vel < self.player.max_speed:
+                self.player.left_vel += 1
+            else:
+                self.player.left_vel = self.player.max_speed
+        else:
+            if self.player.left_vel > 0:
+                self.player.left_vel -= 1
+            else:
+                self.player.left_vel = 0
+        self.player.move_left()
+
+    def on_key_press(self, key, modifiers):
+        if key == pyglet.window.key.UP:
+            self.key_holder['Up'] = True
+        elif key == pyglet.window.key.DOWN:
+            self.key_holder['Down'] = True
+        elif key == pyglet.window.key.RIGHT:
+            self.key_holder['Right'] = True
+        elif key == pyglet.window.key.LEFT:
+            self.key_holder['Left'] = True
+        elif key == pyglet.window.key.ESCAPE:
+            pyglet.app.exit()
+
+    def on_key_release(self, key, modifiers):
+        if key == pyglet.window.key.UP:
+            self.key_holder['Up'] = False
+        elif key == pyglet.window.key.DOWN:
+            self.key_holder['Down'] = False
+        elif key == pyglet.window.key.RIGHT:
+            self.key_holder['Right'] = False
+        elif key == pyglet.window.key.LEFT:
+            self.key_holder['Left'] = False
 
 
 def rgb_to_pyglet(list):
-	result = []
-	for i in list:
-		result.append(i/255)
-	return result
+    result = []
+    for i in list:
+        result.append(i/255)
+    return result
 
 
 if __name__ == '__main__':
-	window = Window()
-	pyglet.app.run()
+    window = Window()
+    pyglet.app.run()
